2f2f5847f328a8ed29cf930ff373c4b6ca026d8a
Christian Pajung <christian.pajung@telekom.de>
2019-03-12 15:13:38 +0100

    end of lesson 3 - Styling & Layout

--- components/History.js
+++ components/History.js
@@ -1,42 +1,115 @@
 import React, { Component } from "react";
-import { View, Text, TouchableOpacity, AsyncStorage } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Platform,
+  TouchableOpacity
+} from "react-native";
 import { connect } from "react-redux";
 import { receiveEntries, addEntry } from "../actions";
 import { timeToString, getDailyReminderValue } from "../utils/helpers";
 import { fetchCalendarResults } from "../utils/api";
+import UdaciFitnessCalendar from "udacifitness-calendar";
+import DateHeader from "./DateHeader";
+import { white } from '../utils/colors';
+import MetricCard from './MetricCard';
+import { AppLoading} from 'expo';
 
 class History extends Component {
-  componentDidMount() {
-    const { dispatch } = this.props;
+  state = {
+    ready: false,
+  }
+  componentDidMount () {
+    const { dispatch } = this.props
+
     fetchCalendarResults()
-      .then(entries => dispatch(receiveEntries(entries)))
+      .then((entries) => dispatch(receiveEntries(entries)))
       .then(({ entries }) => {
         if (!entries[timeToString()]) {
-          dispatch(addEntry({ [timeToString()]: getDailyReminderValue() }));
+          dispatch(addEntry({
+            [timeToString()]: getDailyReminderValue()
+          }))
         }
-      });
+      })
+      .then(() => this.setState(() => ({ready: true})))
   }
 
-  clearAsyncStorage() {
-    AsyncStorage.clear();
-  }
+  // renderItem receives an object, which is a day's state, or the result of
+  // getDailyReminderValue ( which is {today: "👋 Don't forget to log your data today!"})
+  // this object is a day, which comes out of the redux store
+  renderItem = ({ today, ...metrics }, formattedDate, key) => (
+    <View style={styles.item}>
+      {today
+        ? <View>
+            <DateHeader date={formattedDate}/>
+            <Text style={styles.noDataText}>
+              {today}
+            </Text>
+          </View>
+        : <TouchableOpacity
+            onPress={() => console.log('Pressed!')}
+          >
+            <MetricCard date={formattedDate} metrics={metrics} />
+          </TouchableOpacity>}
+    </View>
+  )
 
-  render() {
+  // renderEmptyDate is called, if a day out of the redux store is null
+
+  renderEmptyDate(formattedDate) {
     return (
-      <View>
-        <TouchableOpacity onPress={this.clearAsyncStorage}>
-          <Text>Reset AsyncStorage</Text>
-        </TouchableOpacity>
-        <Text>{JSON.stringify(this.props)}</Text>
+      <View style={styles.item}>
+        <DateHeader date={formattedDate}/>
+        <Text style={styles.noDataText}>
+          You didn't log any data on this day.
+        </Text>
       </View>
-    );
+    )
+  }
+  render() {
+    const { entries } = this.props
+    const { ready } = this.state
+    if (ready === false) {
+      return <AppLoading />
+    }
+    return (
+      <UdaciFitnessCalendar
+        items={entries}
+        renderItem={this.renderItem}
+        renderEmptyDate={this.renderEmptyDate}
+      />
+    )
   }
 }
-
-function mapStateToProps(entries) {
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: white,
+    borderRadius: Platform.OS === 'ios' ? 16 : 2,
+    padding: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 17,
+    justifyContent: 'center',
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowColor: 'rgba(0, 0, 0, 0.24)',
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
+  },
+  noDataText: {
+    fontSize: 20,
+    paddingTop: 20,
+    paddingBottom: 20
+  }
+})
+function mapStateToProps (entries) {
   return {
     entries
-  };
+  }
 }
-
-export default connect(mapStateToProps)(History);
+export default connect(
+  mapStateToProps,
+)(History)
